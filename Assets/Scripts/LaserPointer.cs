844b563d730077c11129c9f164ecc04525ccd2ab
Michael Makogon <michael.makogon@gmail.com>
2019-05-13 00:45:29 -0400

    Created node building system
    Changed the building system to use nodes instead of free placement.

--- Assets/Scripts/LaserPointer.cs
+++ Assets/Scripts/LaserPointer.cs
@@ -17,22 +17,20 @@ public class LaserPointer : MonoBehaviour
     public LayerMask teleportMask;
     public int maxHitDistance;
 
-    private GameObject reticle;
-    private Transform teleportReticleTransform;
     private bool shouldShow, shouldSpawn, readyToPress;
     private GameObject laser;
     private Transform laserTransform;
     private Vector3 hitPoint;
+    private TowerNode nodeSelected;
     private GameObject spawnPrefab;
+    private TowerNode nodeHighlighted;
+    private Color previousColor;
 
     // Start is called before the first frame update
     void Start()
     {
         laser = Instantiate(laserPrefab);
         laserTransform = laser.transform;
-
-        reticle = Instantiate(teleportReticlePrefab);
-        teleportReticleTransform = reticle.transform;
     }
 
     // Update is called once per frame
@@ -40,55 +38,93 @@ public class LaserPointer : MonoBehaviour
     {
         if (shouldShow)
         {
-            RaycastHit hit;
-            
-            if (Physics.Raycast(controllerPose.transform.position, transform.forward, out hit, maxHitDistance, teleportMask)) // hit valid target
+            if (Physics.Raycast(controllerPose.transform.position, transform.forward, out RaycastHit hit, maxHitDistance, teleportMask)) // hit valid target
             {
-                hitPoint = hit.point;
+                TowerNode node = hit.collider.GetComponent<TowerNode>();
                 
-                Renderer rend = laser.GetComponent<Renderer>();
-                rend.material.shader = Shader.Find("Unlit/Color");
-                rend.material.SetColor("_Color", Color.green);
+                if(node != null)
+                {
+                    Renderer nodeRend = node.GetComponent<Renderer>();
 
-                ShowLaser(hit.distance);
+                    if(nodeHighlighted == null)
+                    {
+                        nodeHighlighted = node;
+                        previousColor = nodeRend.material.GetColor("_Color");
+                    }
+                    else if (node != nodeHighlighted) // if a new node is being pointed at
+                    {
+                        Renderer nodeHighRend = nodeHighlighted.GetComponent<Renderer>();
+                        nodeHighRend.material.SetColor("_Color", previousColor);
+                        // get pointers to that node/color
+                        nodeHighlighted = node;
+                        previousColor = nodeRend.material.GetColor("_Color");
+                    }
 
-                reticle.SetActive(true);
-                teleportReticleTransform.position = hitPoint + teleportReticleOffset;
-                shouldSpawn = true;
+                    if (node.occupied)
+                    {
+                        nodeRend.material.SetColor("_Color", Color.red);
+                        shouldSpawn = false;
+                    }
+                    else
+                    {
+                        nodeRend.material.SetColor("_Color", Color.green);
+                        shouldSpawn = true;
+                    }
+
+                    hitPoint = hit.point;
+                    nodeSelected = hit.collider.GetComponent<TowerNode>();
+
+                    Renderer rend = laser.GetComponent<Renderer>();
+                    rend.material.shader = Shader.Find("Unlit/Color");
+                    rend.material.SetColor("_Color", Color.green);
+
+                    ShowLaser(hit.distance);
+                }
             }
             else if (Physics.Raycast(controllerPose.transform.position, transform.forward, out hit, maxHitDistance)) // hit invalid target 
             {
+                if (nodeHighlighted != null)
+                {
+                    Renderer nodeRend = nodeHighlighted.GetComponent<Renderer>();
+                    nodeRend.material.SetColor("_Color", previousColor);
+                    nodeHighlighted = null;
+                }
+
                 hitPoint = hit.point;
+                nodeSelected = null;
 
                 Renderer rend = laser.GetComponent<Renderer>();
                 rend.material.shader = Shader.Find("Unlit/Color");
                 rend.material.SetColor("_Color", Color.red);
 
                 ShowLaser(hit.distance);
-
-                reticle.SetActive(true);
-                teleportReticleTransform.position = hitPoint + teleportReticleOffset;
+                
                 shouldSpawn = false;
             }
             else // hit air
             {
+                if(nodeHighlighted != null)
+                {
+                    Renderer nodeRend = nodeHighlighted.GetComponent<Renderer>();
+                    nodeRend.material.SetColor("_Color", previousColor);
+                    nodeHighlighted = null;
+                }
+
                 hitPoint = controllerPose.transform.position + transform.forward * 10;
+                nodeSelected = null;
 
                 Renderer rend = laser.GetComponent<Renderer>();
                 rend.material.shader = Shader.Find("Unlit/Color");
                 rend.material.SetColor("_Color", Color.red);
 
                 ShowLaser(maxHitDistance);
-
-                reticle.SetActive(true);
-                teleportReticleTransform.position = hitPoint + teleportReticleOffset;
+                
                 shouldSpawn = false;
             }
         }
         else
         {
             laser.SetActive(false);
-            reticle.SetActive(false);
         }
     }
 
@@ -113,16 +149,25 @@ public class LaserPointer : MonoBehaviour
     {
         if (shouldSpawn)
         {
-            Instantiate(spawnPrefab, hitPoint, Quaternion.identity);
+            nodeSelected?.SpawnTower(spawnPrefab);
         }
 
         Deactivate();
     }
 
     public void Deactivate()
-    { 
+    {
+        if (nodeHighlighted != null)
+        {
+            Renderer nodeRend = nodeHighlighted.GetComponent<Renderer>();
+            nodeRend.material.SetColor("_Color", previousColor);
+            nodeHighlighted = null;
+        }
+
         shouldShow = false;
         shouldSpawn = false;
-        reticle.SetActive(false);
     }
 }
+
+// TODO: make laser red when pointing at occupied nodes
+// TODO: give haptic feedback when good/bad placement
---
+++ Assets/Scripts/TowerNode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerNode : MonoBehaviour
+{    
+    [HideInInspector]
+    public bool occupied;
+
+    private Vector3 spawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        occupied = false;
+        Transform tform = this.GetComponent<Transform>();
+        spawnPosition = tform.position + new Vector3(0, tform.localScale.y / 2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnTower(GameObject towerPrefab)
+    {
+        Instantiate(towerPrefab, spawnPosition, Quaternion.identity);
+        occupied = true;
+    }
+}
