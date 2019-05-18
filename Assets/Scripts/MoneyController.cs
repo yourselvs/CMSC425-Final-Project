77d7b08092a732707c7a6dc1a96dde931b95a981
Michael Makogon <michael.makogon@gmail.com>
2019-05-18 19:53:18 -0400

    added currency assets to level 1

--- Assets/Scripts/MoneyController.cs
+++ Assets/Scripts/MoneyController.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class MoneyController : MonoBehaviour
 {
-    public Text moneyText1, moneyText2;
+    public Text moneyText1, moneyText2,
+                moneyText3, moneyText4;
     public int money;
 
     // Start is called before the first frame update
@@ -20,5 +21,7 @@ public class MoneyController : MonoBehaviour
         string text = "$" + money;
         moneyText1.text = text;
         moneyText2.text = text;
+        moneyText3.text = text;
+        moneyText4.text = text;
     }
 }
